d4d52a710edb39889a755b1348b6ea22e23d422f
Mahi Dodiya <mahidodiya76@gmail.com>
2026-01-08 23:29:35 +0530

    Add files via upload

---
+++ 5_array_methods.py
@@ -0,0 +1,16 @@
+import numpy as np
+arr1=np.array([[1,2,3],[4,5,6]])#2d array
+print(arr1)
+#create array using zeros
+arr2=np.zeros(5)
+print(arr2)
+#create array using ones
+arr3=np.ones(10)
+print(arr3)
+#create array using range
+arr4=np.arange(10)
+print(arr4)
+arr5=np.arange(10,20)
+print(arr5)
+arr6=np.arange(10,20,2)
+print(arr6)
