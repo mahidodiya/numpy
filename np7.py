ab83def460fa91e7ca82567a940c7efcd973c261
Mahi Dodiya <mahidodiya76@gmail.com>
2026-01-22 20:27:27 +0530

    Add files via upload

---
+++ np7.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+#shape
+arr=np.array([1,2,3,4,5])
+arr1=np.array([[1,2,3],[4,5,6]])
+arr2=np.array([[[1,2,3],[4,5,6]],[[1,2,3],[4,5,6]],[[1,2,3],[4,5,6]]])
+
+print(arr)
+print(arr.shape)
+print(arr1)
+print(arr1.shape)
+print(arr2)
+print(arr2.shape)
